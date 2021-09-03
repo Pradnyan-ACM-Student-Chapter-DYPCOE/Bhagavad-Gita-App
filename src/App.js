8c3426b3a70c48347e7917c5a30cf5f609b438eb
kothariji <kotharidhruv25@gmail.com>
2021-09-03 22:42:22 +0530

    workshop ready

--- src/App.js
+++ src/App.js
@@ -6,29 +6,10 @@ import "./App.css";
 import axios from "axios";
 
 const App = () => {
-  const [quote, setQuote] = useState("Click on GENERATE RANDOM QUOTE button to get a quote");
-
-  const handleClick = async () => {
-    await axios
-      .get("https://bhagavad-gita-quote-api.herokuapp.com/quotes/random")
-      .then((response) => setQuote(response.data))
-      .catch((err) => console.log(err));
-  };
 
   return (
-    <div className="main_div">
-      <Navbar />
-      <div className="paper_container">
-        <img src={old_paper} alt="Old Paper" className="old_paper" />
-        <img
-          src={bhagawadGita}
-          alt="Bhagawad Gita Logo "
-          className="bhagawadGita"
-        />
-        <p>{quote}</p>
-        <button onClick={handleClick}>GENERATE RANDOM QUOTE</button>
-      </div>
-      <div className="generateQuote"></div>
+    <div>
+      Hello World
     </div>
   );
 }
